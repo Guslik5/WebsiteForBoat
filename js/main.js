88ddd203ef574393d71e3f235f92529d76cbfc4f
Dmitriy <guslik2003@mail.ru>
2023-11-25 14:22:44 +0300

    added table with boat description

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+
+
+if (document.documentElement.scrollHeight !== document.documentElement.offsetHeight) {
+    console.log("Был скрол")
+    console.log(document.documentElement.scrollHeight)
+    console.log(document.documentElement.offsetHeight)
+}

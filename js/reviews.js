1fb7e323b360b6dab5bca1c87c926936134bd9f0
Dmitriy <guslik2003@mail.ru>
2024-02-17 15:28:22 +0300

    fix lab5

--- js/reviews.js
+++ js/reviews.js
@@ -11,12 +11,22 @@ window.addEventListener('DOMContentLoaded', function() {
     formReviews.onsubmit = function (event){
         event.preventDefault();
 
+        let formData = new FormData(event.target);
+
         let row = tableReviews.insertRow();
-        row.insertCell().textContent = event.target[0].value;
-        row.insertCell().textContent = event.target[1].value;
-        row.insertCell().textContent = event.target[2].value;
-        row.insertCell().textContent = event.target[3].value;
-        row.insertCell().textContent = event.target[4].value;
+        row.insertCell().textContent = formData.get('name');
+        row.insertCell().textContent = formData.get('nameBoat');
+        row.insertCell().textContent = formData.get('nameCaptain');
+        row.insertCell().textContent = formData.get('comment');
+        row.insertCell().textContent = formData.get('rating');
+
+
+        // let row = tableReviews.insertRow();
+        // row.insertCell().textContent = event.target[0].value;
+        // row.insertCell().textContent = event.target[1].value;
+        // row.insertCell().textContent = event.target[2].value;
+        // row.insertCell().textContent = event.target[3].value;
+        // row.insertCell().textContent = event.target[4].value;
 
         localStorage.setItem("Data", tableReviews.innerHTML);
     }
